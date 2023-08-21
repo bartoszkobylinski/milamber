f345bee233eecffa6921b7bb13944dc1d71a6127
bartoszkobylinski <bartosz.kobylinski@gmail.com>
2023-08-21 11:41:47 +0200

    Title: Enhance Milamber UI and Add Webhook Integration
    Description:
    
    Added necessary imports for the webhook integration and the updated UI (requests, os, QVBoxLayout).
    Modified the MainWindow class to include:
    Retrieval of the MILAMBER_WEBHOOK environment variable.
    A restructured GUI layout using QVBoxLayout for a clearer interface.
    A "Send" button that triggers the send_to_milamber_webhook method, responsible for sending a message to the Milamber webhook.
    Proper handling of the webhook response, providing feedback based on the status code received.
    Updated the application initialization to be under the if __name__ == "__main__": clause for better practice.

--- main.py
+++ main.py
@@ -1,20 +1,43 @@
-from PyQt6.QtWidgets import QApplication, QWidget, QMainWindow, QPushButton
+import requests
+from PyQt6.QtWidgets import QApplication, QWidget, QMainWindow, QPushButton, QVBoxLayout
 import sys
+import os
 
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-
+        self.milamber_webhook = os.getenv("MILAMBER_WEBHOOK")
         self.setWindowTitle("Milamber")
-        button = QPushButton("Press Me!")
 
-        self.setCentralWidget(button)
+        central_widget = QWidget(self)
+        layout = QVBoxLayout()
+        button = QPushButton("Send")
+        button.clicked.connect(self.send_to_milamber_webhook)
+        layout.addWidget(button)
+        central_widget.setLayout(layout)
+        self.setCentralWidget(central_widget)
+
+    def send_to_milamber_webhook(self):
+        url = self.milamber_webhook
+
+        data = {
+            "query": "Hi, from the other side?",
+            "type": "query"
+        }
+
+        response = requests.post(url, json=data)
+        if response.status_code == 200:
+            print("Succesfully sent data to webhook")
+        else:
+            print(f"Something went wrong: {response.status_code}")
 
 
-app = QApplication(sys.argv)
+if __name__ == "__main__":
 
-window = MainWindow()
-window.show()
+    app = QApplication(sys.argv)
 
-app.exec()
+    window = MainWindow()
+    window.show()
+
+    app.exec()
