78e5f16059d7f93403bc7cfbb4846d1a483e6f5f
bartoszkobylinski <bartosz.kobylinski@gmail.com>
2023-08-21 17:38:38 +0200

    Title: Enhance UI and code structure in main.py
    Description:
    
    Integrated MILAMBER_WEBHOOK directly from secret_data for improved code clarity.
    Expanded Qt imports to support enhanced UI components.
    Introduced ChatItem class for a modular chat UI component.
    Added functionality to show an icon next to Milamber responses.
    Utilized QTextEdit for chat messages to improve UI look and readability.
    Replaced response label with a dynamic chat list (QListWidget), offering a more intuitive chat experience.
    Added error handling in send_to_milamber_webhook method to improve user feedback.
    Set a fixed window size for a better UI experience.

--- main.py
+++ main.py
@@ -1,61 +1,92 @@
 import requests
-from PyQt6.QtWidgets import QApplication, QWidget, QMainWindow, QPushButton, QVBoxLayout, QLineEdit, QLabel
-from PyQt6.QtGui import QIcon
+from secret_data import MILAMBER_WEBHOOK
+from PyQt6.QtWidgets import (QApplication, QWidget, QMainWindow, QPushButton, QVBoxLayout, QLineEdit, QScrollArea,
+                             QTextEdit, QHBoxLayout, QListWidget, QLabel, QListWidgetItem)
+from PyQt6.QtGui import QIcon, QPixmap
 import sys
-import os
-from dotenv import load_dotenv
 
-load_dotenv(".secret")
 
-MILAMBER_ICON_PATH = os.getenv("MILAMBER_ICON_PATH")
+class ChatItem(QWidget):
+    def __init__(self, text, is_milamber=False):
+        super().__init__()
+        layout = QHBoxLayout(self)
+
+        if is_milamber:
+            icon_label = QLabel(self)
+            pixmap = QPixmap("milamber.icns")
+            icon_label.setPixmap(pixmap.scaled(32, 32))
+            layout.addWidget(icon_label)
+
+        text_label = QTextEdit(self)
+        text_label.setPlainText(text)
+        text_label.setReadOnly(True)
+        layout.addWidget(text_label)
+
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.milamber_webhook = os.getenv("MILAMBER_WEBHOOK")
+        self.milamber_webhook = MILAMBER_WEBHOOK
         self.setWindowTitle("Milamber")
-        self.setWindowIcon(QIcon(MILAMBER_ICON_PATH))
+        self.setWindowIcon(QIcon("milamber.icns"))
 
         central_widget = QWidget(self)
         layout = QVBoxLayout()
+
+        self.chat_list = QListWidget(self)
+        layout.addWidget(self.chat_list)
 
         # Adding QLineEdit for user input
         self.input_field = QLineEdit(self)
         layout.addWidget(self.input_field)
 
-        button = QPushButton("Send")
+        button = QPushButton("Send", self)
         button.clicked.connect(self.send_to_milamber_webhook)
         layout.addWidget(button)
-
-        self.response_label = QLabel("Response will appear here", self)
-        layout.addWidget(self.response_label)
 
         central_widget.setLayout(layout)
         self.setCentralWidget(central_widget)
 
     def send_to_milamber_webhook(self):
-        query_text = self.input_field.text()
+        user_text = self.input_field.text()
+
+        user_item = QListWidgetItem(self.chat_list)
+        user_widget = ChatItem(user_text)
+        user_item.setSizeHint(user_widget.sizeHint())
+        self.chat_list.addItem(user_item)
+        self.chat_list.setItemWidget(user_item, user_widget)
+
         url = self.milamber_webhook
 
         data = {
-            "query": query_text,
+            "query": user_text,
             "type": "query"
         }
         try:
             response = requests.post(url, json=data)
             response_data = response.json()
             if response.status_code == 200:
-                self.response_label.setText(f"Response: {response_data.get('answer')}")
+                milamber_text = response_data.get('answer')
             else:
-                self.response_label.setText(f"Error {response.status_code}")
+                milamber_text = response.status_code
+
+            milamber_item = QListWidgetItem(self.chat_list)
+            milamber_widget = ChatItem(milamber_text, is_milamber=True)
+            milamber_item.setSizeHint(milamber_widget.sizeHint())
+            self.chat_list.addItem(milamber_item)
+            self.chat_list.setItemWidget(milamber_item, milamber_widget)
+
+            self.input_field.clear()
+
         except requests.RequestException as error:
-            self.response_label.setText(f"Network error {error}")
+            self.response_text_edit.setText(f"Network error {error}")
 
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
 
     window = MainWindow()
+    window.setFixedSize(500, 400)
     window.show()
 
     app.exec()
