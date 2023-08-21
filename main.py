a52aaad1b5abf94ed62ffa4683dd43e1a48f44e3
bartoszkobylinski <bartosz.kobylinski@gmail.com>
2023-08-21 13:03:37 +0200

    Title: Integrate dotenv and Enhance Milamber UI
    Description:
    
    Integrated the dotenv module to read sensitive data from a .secret file.
    Retrieved the path for the Milamber icon (MILAMBER_ICON_PATH) from .secret and set it as the window icon.
    Introduced new UI elements:
    QLineEdit to allow user input for the query text.
    QLabel to display the response or any error messages from the webhook call.
    Updated the send_to_milamber_webhook method:
    Uses the text from the QLineEdit as the query.
    Improved error handling using a try-except block to handle network-related exceptions.
    Displays the response or error directly on the GUI rather than the console.
    Added .gitignore to the repository to prevent committing sensitive files like .secret.

--- main.py
+++ main.py
@@ -1,40 +1,58 @@
 import requests
-from PyQt6.QtWidgets import QApplication, QWidget, QMainWindow, QPushButton, QVBoxLayout
+from PyQt6.QtWidgets import QApplication, QWidget, QMainWindow, QPushButton, QVBoxLayout, QLineEdit, QLabel
+from PyQt6.QtGui import QIcon
 import sys
 import os
+from dotenv import load_dotenv
 
+load_dotenv(".secret")
+
+MILAMBER_ICON_PATH = os.getenv("MILAMBER_ICON_PATH")
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.milamber_webhook = os.getenv("MILAMBER_WEBHOOK")
         self.setWindowTitle("Milamber")
+        self.setWindowIcon(QIcon(MILAMBER_ICON_PATH))
 
         central_widget = QWidget(self)
         layout = QVBoxLayout()
+
+        # Adding QLineEdit for user input
+        self.input_field = QLineEdit(self)
+        layout.addWidget(self.input_field)
+
         button = QPushButton("Send")
         button.clicked.connect(self.send_to_milamber_webhook)
         layout.addWidget(button)
+
+        self.response_label = QLabel("Response will appear here", self)
+        layout.addWidget(self.response_label)
+
         central_widget.setLayout(layout)
         self.setCentralWidget(central_widget)
 
     def send_to_milamber_webhook(self):
+        query_text = self.input_field.text()
         url = self.milamber_webhook
 
         data = {
-            "query": "Hi, from the other side?",
+            "query": query_text,
             "type": "query"
         }
-
-        response = requests.post(url, json=data)
-        if response.status_code == 200:
-            print("Succesfully sent data to webhook")
-        else:
-            print(f"Something went wrong: {response.status_code}")
+        try:
+            response = requests.post(url, json=data)
+            response_data = response.json()
+            if response.status_code == 200:
+                self.response_label.setText(f"Response: {response_data.get('answer')}")
+            else:
+                self.response_label.setText(f"Error {response.status_code}")
+        except requests.RequestException as error:
+            self.response_label.setText(f"Network error {error}")
 
 
 if __name__ == "__main__":
-
     app = QApplication(sys.argv)
 
     window = MainWindow()
